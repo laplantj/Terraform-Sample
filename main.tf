6c0fcdeb5275e82a3e851c18eff038495d318f51
Josh Laplante <33742797+laplantj@users.noreply.github.com>
2022-06-06 14:25:02 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "myawsserver1" {
   ami = "ami-0603cbe34fd08cb81"
   instance_type = var.hw
   tags = {
-    Name = "gagan-vm1"
+    Name = "josh-vm11"
     Env = "Dev"
   }
 }
