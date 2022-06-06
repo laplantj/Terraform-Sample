605e834487159be8ce7fea17c94542845eab3281
Josh Laplante <33742797+laplantj@users.noreply.github.com>
2022-06-06 14:54:29 -0500

    remove-infra

--- main.tf
+++ main.tf
@@ -1,17 +1,1 @@
-provider "aws" {
-  region = "us-east-2"
-}
 
-variable "hw" {
-type = string
-default = "t2.nano"
-}
-
-resource "aws_instance" "myawsserver1" {
-  ami = "ami-0603cbe34fd08cb81"
-  instance_type = var.hw
-  tags = {
-    Name = "josh-vm11-change3"
-    Env = "Prod"
-  }
-}
